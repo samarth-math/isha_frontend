5df62fedf004143a0b2757f412bf3c880a69da98
sam <samarth.math@gmail.com>
2019-11-17 08:44:02 -0800

    meditator fields populated

--- src/components/EditableUser.vue
+++ src/components/EditableUser.vue
@@ -1,19 +1,18 @@
 <template>
   <div>
-    Is this you?
     <b-form @submit="onSubmit">
       First Name : <b-form-input v-model="meditator.firstname"></b-form-input>
-      Last Name: <b-form-input v-model="meditator.firstname"></b-form-input>
-      Phone : <b-form-input v-model="meditator.firstname"></b-form-input>
-      Country : <b-form-input v-model="meditator.firstname"></b-form-input>
-      ZipCode : <b-form-input v-model="meditator.firstname"></b-form-input>
+      Last Name: <b-form-input v-model="meditator.lastname"></b-form-input>
+      Phone : <b-form-input v-model="meditator.phone"></b-form-input>
+      Country : <b-form-input v-model="meditator.mailingcountry"></b-form-input>
+      ZipCode : <b-form-input v-model="meditator.cdi_mailingpostalcode__c"></b-form-input>
 
-      {{picked}}
       <b-button
         type="submit"
         variant="primary"
       >Submit</b-button>
     </b-form>
+    {{picked}}
   </div>
 </template>
 
