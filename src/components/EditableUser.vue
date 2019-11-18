42f9eb025ba64981ee27bd27544b557ae51d9d6d
sam <samarth.math@gmail.com>
2019-11-17 18:29:55 -0800

    Disabled form with open checkout date field

--- src/components/EditableUser.vue
+++ src/components/EditableUser.vue
@@ -1,12 +1,36 @@
 <template>
   <div>
     <b-form @submit="onSubmit">
-      First Name : <b-form-input v-model="meditator.firstname"></b-form-input>
-      Last Name: <b-form-input v-model="meditator.lastname"></b-form-input>
-      Phone : <b-form-input v-model="meditator.phone"></b-form-input>
-      Country : <b-form-input v-model="meditator.mailingcountry"></b-form-input>
-      ZipCode : <b-form-input v-model="meditator.cdi_mailingpostalcode__c"></b-form-input>
-      Current Visit Checkin Date : <b-form-input v-model="visitInfo.checkin_date"></b-form-input>
+      First Name : <b-form-input
+        disabled="disabled"
+        v-model="meditator.firstname"
+      ></b-form-input>
+      Last Name: <b-form-input
+        disabled="disabled"
+        v-model="meditator.lastname"
+      ></b-form-input>
+      Email: <b-form-input
+        disabled="disabled"
+        v-model="meditator.email"
+      ></b-form-input>
+      Phone : <b-form-input
+        disabled="disabled"
+        v-model="meditator.phone"
+      ></b-form-input>
+      Country : <b-form-input
+        disabled="disabled"
+        v-model="meditator.mailingcountry"
+      ></b-form-input>
+      ZipCode : <b-form-input
+        disabled="disabled"
+        v-model="meditator.cdi_mailingpostalcode__c"
+      ></b-form-input>
+      Current Visit Checkin Date : <b-form-input
+        class="form-control"
+        type="datetime-local"
+        disabled="disabled"
+        v-model="visitInfo.checkin_date"
+      ></b-form-input>
       Current Visit Checkout Date : <b-form-input v-model="visitInfo.checkout_date"></b-form-input>
 
       <b-button
@@ -25,7 +49,7 @@ export default {
     return {
       picked: '',
       visitInfo: {
-        checkin_date: new Date().toLocaleDateString(),
+        checkin_date: new Date(new Date().toString().split('GMT')[0] + ' UTC').toISOString().split('.')[0],
         checkout_date: ''
       }
     }
